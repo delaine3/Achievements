2d92c5810537578b2766500ce0678caf23ecada8
Delaine Abner <50344531+delaine3@users.noreply.github.com>
2022-02-24 17:23:56 -0500

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
 import Link from 'next/link'
 import dbConnect from '../lib/dbConnect'
 import Achievement from '../models/Achievement'
+import React, { useEffect, useState } from "react";
 
 export function theDate() {
   const date = new Date();
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + " " +date.toLocaleDateString(undefined, options) + "\n"
 }
-export function newDay(){
-  return <div>Hey</div>
-}
+
+
 export function checkDate(achievementDate){
   let parseDate =  parseInt(achievementDate)
 
-  let achievementDateAsDate = new Date(parseDate).getDate()
-  //console.log( achievementDateAsDate == new Date().getDate())
+  let currA= new Date(parseDate).getDate()
+  //console.log( currA== new Date().getDate())
 
-  return achievementDateAsDate == new Date().getDate()
+  return currA== new Date().getDate()
 
 }
 export function checkIfYesterday(achievementDate){
+
  // console.log("achievementDate is " + achievementDate)
   //console.log("achievementDate is a " +typeof(achievementDate))
 
@@ -28,26 +29,84 @@ export function checkIfYesterday(achievementDate){
   //console.log("parseDate is a " +typeof(parseDate))
  // console.log("parseDate is a " + parseDate)
 
-  let achievementDateAsDate = new Date(parseDate).getDate() + 1
+  let currA= new Date(parseDate).getDate() + 1
 
-//console.log("achievementDateAsDate " + achievementDateAsDate)
+//console.log("currA" + currA
 //console.log("***************************************************")
-  return achievementDateAsDate == new Date().getDate()
+  return currA== new Date().getDate()
 
 }
 
 function Index ({ achievements }) {
-  function styleObj (){
-    var color = Array("#e9d8a6", "#ee9b00", "#94d2bd", "#0a9396", "#ca6702", "#9b2226");
-    const newStyle={
-      backgroundColor: color[Math.floor(Math.random() * color.length)],
+  const [previous, setPrevious] = useState("2018, 11, 24, 10, 33, 30, 0");
+ 
+function setPreviosDate(insertDate){
+  setPrevious(insertDate)
+
+}
+  function newDay(achievement){
+      
+    const [currStyle, setCurrStyle] = useState({
+      backgroundColor: "#ee9b00",
       textAlign: "center",
       width: "90%",
       fontSize: "25px",
-      marginLeft: "4%"
-     }
-    return newStyle
-}
+      marginLeft: "4%"});
+  
+    let parseCurrAchiementDate =  parseInt(achievement.insertDate)
+    let currAchievementDateAsDate = new Date(parseCurrAchiementDate)
+  
+    let parsePrevAchiementDate =  parseInt(previous)
+    let prevAchiementAsDate= new Date(parsePrevAchiementDate)
+  
+    function datesAreOnSameDay (first, second){
+      {()=>setPreviosDate(achievement.insertDate)}
+    return first.getFullYear() === second.getFullYear() &&
+    first.getMonth() === second.getMonth() &&
+    first.getDate() === second.getDate();
+    }
+    function styleObj (){
+      const newStyle={
+        textAlign: "center",
+        width: "90%",
+        fontSize: "25px",
+        marginLeft: "4%",
+        padding:"10px "
+       }
+      return newStyle
+  }
+  console.log(currAchievementDateAsDate + " " + prevAchiementAsDate)
+  console.log(datesAreOnSameDay(currAchievementDateAsDate, prevAchiementAsDate))
+  
+    
+    return <div  key={achievement.insertDate }> 
+    <div key={achievement._id}>
+    <div  key={achievement._id} className="card">
+      <img src={achievement.image_url} />
+      <h5 className="achievement-name"> {achievement.name}</h5>
+      <h3 style={styleObj() }>{achievement.theDate.slice(18)}</h3>
+
+      <br/>
+      <div className="main-content">
+        <p className="achievement-name">{achievement.name}</p>
+        <br/>
+        <p className="details">Details: {achievement.achievement_details.split(" ").splice(0,10).join(" ")}</p>
+        <br/>
+        <p className="details">Date: {achievement.theDate}</p>
+        <div className="btn-container">
+          <Link href="/[id]/edit" as={`/${achievement._id}/edit`}>
+            <button className="btn edit">Edit</button>
+          </Link>
+          <Link href="/[id]" as={`/${achievement._id}`}>
+            <button className="btn view">View</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  </div> </div>
+    
+  }
+ 
 function styleObj2 (){
   const newStyle={
     marginTop: "0%"
@@ -132,28 +191,10 @@ function styleObj2 (){
     {achievements.map((achievement) => (
       !checkIfYesterday(achievement.insertDate) && !checkIfYesterday(achievement.insertDate) ?(
         
-      <div key={achievement._id}>
-        <h3  style={styleObj()}>{achievement.theDate.slice(8)}</h3>
-        <div  key={achievement._id} className="card">
-          <img src={achievement.image_url} />
-          <h5 className="achievement-name"> {achievement.name}</h5>
-          <br/>
-          <div className="main-content">
-            <p className="achievement-name">{achievement.name}</p>
-            <br/>
-            <p className="details">Details: {achievement.achievement_details.split(" ").splice(0,10).join(" ")}</p>
-            <br/>
-            <p className="details">Date: {achievement.theDate}</p>
-            <div className="btn-container">
-              <Link href="/[id]/edit" as={`/${achievement._id}/edit`}>
-                <button className="btn edit">Edit</button>
-              </Link>
-              <Link href="/[id]" as={`/${achievement._id}`}>
-                <button className="btn view">View</button>
-              </Link>
-            </div>
-          </div>
-        </div>
+      <div>
+        { newDay(achievement) }
+        
+     
       </div>
     ): null) )}
       </div>
